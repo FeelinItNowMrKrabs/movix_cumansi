209f3b56410719db7a69df0799e5ff5bcf349aeb
Petr Iakovlev <iakovlev.pa@domru.ru>
2020-10-03 05:18:56 +0300

    fggfgfgf

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -96,12 +96,22 @@ def get_boevik():
 
 
 
-@app.route('/send_likes', methods=['GET', 'POST'])
-def add_message():
-    content = request.json
-    we_got = request
-    return jsonify({"uuid":"poluchil"})
+@app.route('/send_likes/<string:likes>')
+def add_message(likes):
+    list_like = likes
+    client = Client('127.0.0.1')
+    settt = []
+    hold_me = client.execute(f"""SELECT topK(25)(arrayJoin(sim)) from (select d, arrayFlatten(groupArray(sim)) as sim from (select splitByChar(',', similar_assetids) as sim, today() as d from similarfilms where assetid in ({list_like})) group by d)""")
+    
+    for i in hold_me[0][0]:
+        hold_bitch = client.execute(f"""select assetid, arrayElement(groupArray(title), 1) from events where assetid = {i} group by assetid""")
+        try:
+            tit_id = hold_bitch[0]
+        except IndexError:
+            continue
+        settt.append(tit_id)
+        if len(settt) == 25:
+            break
+    
+    return {'custoized' : settt}
 
-@app.route('/check')
-def check():
-    return {'checked': we_got}
